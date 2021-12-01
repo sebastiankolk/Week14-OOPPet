8795608edffe4915ce3c40db3500acaa81f7c58d
Opilane TTHK <opilane@tthk.ee>
2021-12-01 10:25:23 +0200

    petcar

---
+++ Car/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        class Car
+        {
+            string markname;
+            string modelname;
+            string regnum;
+            string color;
+            int odometer;
+            int fueltank;
+
+            public Car(string _modelname, string _markname,string _regnum, string _color)
+            {
+                modelname = _modelname;
+                markname = _markname;
+                regnum = _regnum;
+                color = _color;
+                odometer = 0;
+                fueltank = 60;
+
+                Console.WriteLine($"{color});
+                
+            }
+            public int Odometer
+            {
+                get { return odometer; }
+            }
+            public int Fueltank
+            {
+                get { return fueltank; }
+            }
+
+            public void Info()
+            {
+                Console.WriteLine($"Modelname: {modelname}. ");
+                Console.WriteLine($"Markname: {markname}");
+                Console.WriteLine($"Registration number: {regnum}");
+                Console.WriteLine($"Color: {color}");
+            }
+            public void GainMeter()
+            {
+                Console.WriteLine("'rrrrrrrrrrr'");
+                odometer += 100;
+            }
+            public void Gas()
+            {
+                Console.WriteLine("'sss'");
+                fueltank -= 5;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car mycar = new Car("modelname", "markname", "123 AAA", "blue");
+            while(mycar.Fueltank <0)
+            { 
+                    mycar.GainMeter();
+                    mycar.Gas();
+                    Console.WriteLine($"Current odometer:{mycar.Odometer}");
+                    Console.WriteLine($"Current fueltank:{mycar.Fueltank}");
+                
+            }
+            mycar.Info();
+        }
+    }
+}
---
+++ OOPPet/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OOPPet
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            double weight;
+            int age;
+
+            public Pet(string _name)
+            {
+                name = _name;
+                weight = 0.3;
+                age = 0;
+                Console.WriteLine($"Congratulations! You've adopted {name}. ");
+            }
+            public double Weight
+            {
+                get { return weight; }
+            }
+
+            public void Info()
+            {
+                Console.WriteLine($"Name: {name}. ");
+                Console.WriteLine($"Age: {age}");
+                Console.WriteLine($"Weight: {weight}");
+            }
+            public void Gain()
+            {
+                Console.WriteLine("'Scrunch, scrunch'");
+                weight += 0.2;
+            }
+            public void Run()
+            {
+                Console.WriteLine("'*heavy breathing*'");
+                weight -= 0.1;
+            }
+            public void GetOlder()
+            {
+                age++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet("Hammu");
+            for (int i = 0; i < 10; i++)
+            {
+                myPet.Gain();
+                Console.WriteLine($"Current weight:{myPet.Weight}");
+                if(myPet.Weight >= 0.7)
+                {
+                    while(myPet.Weight > 0.7)
+                    {
+                        myPet.Run();
+                        Console.WriteLine($"Weight after running: {myPet.Weight}");
+                    }
+                }
+                myPet.GetOlder();
+            }
+            myPet.Info();
+        }
+    }
+}
